f0bed0bfdee8729c551c2d5c942e40a192fb02c5
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-03-18 14:44:47 +0100

    FIRST COMMIT - ADD - html skeleton, js file and link js to html

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Utizizzando i dati forniti creare un arry di oggetti per rappresentare i membri del team
+// MILESTONE 0 creare l'arrai di oggetti
+// MILESTONE 1 stampare su console per ogni membro le informazioni 
+// MILESTONE 2 stampare le stesse informazioni nel DOM come stringhe
+// BONUS 1 trasformare la stringa foto in un immagine effettiva
+// BONUS 2 organizzare i singoli membri in card/schede
+
